9c22f76ad7ac1e02ac90366163ecc53871cfbf57
Sudarshan <107767636+SpSenpai@users.noreply.github.com>
2023-07-30 09:16:56 +0545

    Add files via upload

---
+++ Js/login.js
@@ -0,0 +1,23 @@
+setInterval(()=>{
+    document.querySelector("#next-btn").click()
+}, 6000);
+
+
+document.querySelector("#no-acc").addEventListener('click', ()=>{
+    document.querySelector(".login-register-sec").style.marginLeft = "-100%"
+})
+document.querySelector("#already-acc").addEventListener('click', ()=>{
+    document.querySelector(".login-register-sec").style.marginLeft = "0"
+})
+
+document.querySelector(".forget-pass").addEventListener('click', ()=>{
+    alert("Relax and try to remeber it!")
+})
+
+document.querySelector(".login").addEventListener('click', ()=>{
+    location.href = "Html/homepage.html"
+})
+
+document.querySelector("#register").addEventListener('click', ()=>{
+    location.href = "Html/homepage.html"
+})
